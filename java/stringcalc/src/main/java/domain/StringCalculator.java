ddd9498ba656569bed163fde7dc557251d60389a
Sageniuz <sageniuz@gmail.com>
2015-06-10 15:10:08 +0200

    [Refactoring] Extracted constant for default seperators and regex.

--- java/stringcalc/src/main/java/domain/StringCalculator.java
+++ java/stringcalc/src/main/java/domain/StringCalculator.java
@@ -1,16 +1,19 @@
 package domain;
 
+import static java.lang.String.join;
 import static java.util.Arrays.asList;
 
 public class StringCalculator {
 
     public static final int DEFAULT_VALUE = 0;
+    public static final String DEFAULT_SEPERATORS = join("", asList(",", "\n"));
+    public static final String BY_DEFAULT_SEPERATORS = "[" + DEFAULT_SEPERATORS + "]";
 
     public int Add(String numbers) {
         if (numbers == null || numbers == "")
             return DEFAULT_VALUE;
 
-        int sum = asList(numbers.split("[\n,]"))
+        int sum = asList(numbers.split(BY_DEFAULT_SEPERATORS))
             .stream()
             .mapToInt(s -> Integer.parseInt(s))
             .sum();
