d327c2044d2b0ba18a111ceb0c1840871629c256
Sageniuz <sageniuz@gmail.com>
2015-06-10 14:48:36 +0200

    [Green] Add test 'Add returns sum for coma seperated numbers'

--- java/stringcalc/src/main/java/domain/StringCalculator.java
+++ java/stringcalc/src/main/java/domain/StringCalculator.java
@@ -1,9 +1,18 @@
 package domain;
 
+import static java.util.Arrays.asList;
+
 public class StringCalculator {
+
     public int Add(String numbers) {
         if (numbers == null || numbers == "")
             return 0;
-        return Integer.parseInt(numbers);
+
+        int sum = asList(numbers.split(","))
+            .stream()
+            .mapToInt(s -> Integer.parseInt(s))
+            .sum();
+
+        return sum;
     }
 }
