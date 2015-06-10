5b2f482e3cfdfcf08783437cb56bc84cc76c65f2
ClausPolanka <claus.polanka@chello.at>
2015-06-10 16:08:35 +0200

    [Refactoring] Extract methods and constants

--- java/stringcalc/src/test/java/test/stringcalculator/Test_Add.java
+++ java/stringcalc/src/test/java/test/stringcalculator/Test_Add.java
@@ -51,7 +51,10 @@ public class Test_Add {
         "//;1;2;3 | 6",
         "///1/2/3 | 6",
     }, separator = "[\\|]")
-    public void returns_sum_for_single_character_custom_seperator_seperated_numbers(String givenNumbers, int expectedSum) {
+    public void returns_sum_for_single_character_custom_seperator_seperated_numbers(
+        String givenNumbers,
+        int expectedSum)
+    {
         StringCalculator sut = new StringCalculator();
         int actualSum = sut.Add(givenNumbers);
         assertThat(actualSum).isEqualTo(expectedSum);
