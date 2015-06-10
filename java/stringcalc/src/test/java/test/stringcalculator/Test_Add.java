cb07d38c09a62f3579f2106327143e1b4d490d66
Sageniuz <sageniuz@gmail.com>
2015-06-10 14:30:38 +0200

    [Ignored] Add test for 'Add should return 0 for empty string input'

--- java/stringcalc/src/test/java/test/stringcalculator/Test_Add.java
+++ java/stringcalc/src/test/java/test/stringcalculator/Test_Add.java
@@ -2,6 +2,7 @@ package test.stringcalculator;
 
 import domain.StringCalculator;
 import org.fest.assertions.api.Assertions;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.fest.assertions.api.Assertions.*;
@@ -12,6 +13,14 @@ public class Test_Add {
     public void returns_0_for_null() {
         StringCalculator sut = new StringCalculator();
         int actualSum = sut.Add(null);
+        assertThat(actualSum).isZero();
+    }
+
+    @Test()
+    @Ignore("Automatically will be green. First a failing test!")
+    public void returns_0_for_empty_string() {
+        StringCalculator sut = new StringCalculator();
+        int actualSum = sut.Add("");
         assertThat(actualSum).isZero();
     }
 
