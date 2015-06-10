b414a81cc580a2fbc656963f8502e89603439b29
Sageniuz <sageniuz@gmail.com>
2015-06-10 14:26:46 +0200

    [Red] Add should return 0 for null.

--- java/stringcalc/src/test/java/test/stringcalculator/Test_Add.java
+++ java/stringcalc/src/test/java/test/stringcalculator/Test_Add.java
@@ -1,5 +1,18 @@
 package test.stringcalculator;
 
+import domain.StringCalculator;
+import org.fest.assertions.api.Assertions;
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.*;
+
 public class Test_Add {
+
+    @Test
+    public void returns_0_for_null() {
+        StringCalculator sut = new StringCalculator();
+        int actualSum = sut.Add(null);
+        assertThat(actualSum).isZero();
+    }
 
 }
