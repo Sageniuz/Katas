3158ca030bc1f458031c3ecaf8fc7bdb867d9af3
Sageniuz <sageniuz@gmail.com>
2015-06-10 14:38:14 +0200

    [Refactoring] Switch test to parameterized test via Zohhak framework

--- java/stringcalc/src/test/java/test/stringcalculator/Test_Add.java
+++ java/stringcalc/src/test/java/test/stringcalculator/Test_Add.java
@@ -1,12 +1,16 @@
 package test.stringcalculator;
 
+import com.googlecode.zohhak.api.TestWith;
+import com.googlecode.zohhak.api.runners.ZohhakRunner;
 import domain.StringCalculator;
 import org.fest.assertions.api.Assertions;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.fest.assertions.api.Assertions.*;
 
+@RunWith(ZohhakRunner.class)
 public class Test_Add {
 
     @Test
@@ -24,11 +28,13 @@ public class Test_Add {
         assertThat(actualSum).isZero();
     }
 
-    @Test
-    public void returns_given_number_if_only_single_number_is_provided() {
+    @TestWith({
+        "1, 1"
+    })
+    public void returns_given_number_if_only_single_number_is_provided(String givenNumbers, int expectedSum) {
         StringCalculator sut = new StringCalculator();
-        int actualSum = sut.Add("1");
-        assertThat(actualSum).isEqualTo(1);
+        int actualSum = sut.Add(givenNumbers);
+        assertThat(actualSum).isEqualTo(expectedSum);
     }
 
 }
