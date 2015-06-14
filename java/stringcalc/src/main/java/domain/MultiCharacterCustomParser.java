666a962e8476ea4fb3d0da14611b4b9617d9e2c2
ClausPolanka <claus.polanka@chello.at>
2015-06-14 12:52:48 +0200

    [Refactoring] Moved Parser logic into seperate class. Used Template Method Pattern for custom seperators.

---
+++ java/stringcalc/src/main/java/domain/MultiCharacterCustomParser.java
@@ -0,0 +1,21 @@
+package domain;
+
+class MultiCharacterCustomParser extends Parser {
+
+    public static final String MULTI_CHAR_CUST_SEPERATOR_PREFIX = "//[";
+
+    public MultiCharacterCustomParser(String numbers) {
+        super(numbers);
+    }
+
+    @Override
+    protected String deleteSeperatorPrefixOfNumbers() {
+        return numbers.split("\\]")[1];
+    }
+
+    @Override
+    protected String extractSeperatorOfNumbers() {
+        return numbers.split("\\[")[1].split("\\]")[0];
+    }
+
+}
