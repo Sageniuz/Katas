1b15ce7a07fbe14dd48c1d432ade459813b72c3b
ClausPolanka <claus.polanka@chello.at>
2015-06-14 14:33:22 +0200

    [Test] Added tests for Parser class.

--- java/stringcalc/src/test/java/test/stringcalculator/ParserTest.java
+++ java/stringcalc/src/test/java/test/stringcalculator/ParserTest.java
@@ -1,9 +1,16 @@
 package test.stringcalculator;
 
+import domain.DefaultParser;
+import domain.MultiCharacterCustomParser;
 import domain.Parser;
+import domain.SingleCharacterCustomParser;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+import static domain.MultiCharacterCustomParser.*;
+import static domain.SingleCharacterCustomParser.*;
+import static org.fest.assertions.api.Assertions.assertThat;
 
 public class ParserTest {
     @Rule
@@ -15,5 +22,26 @@ public class ParserTest {
         thrown.expectMessage("numbers");
         String numbers = null;
         Parser.create(numbers);
+    }
+
+    @Test
+    public void returnsCorrectImplementationForGivenNumbers() {
+        String numbers = "";
+        Parser parser = Parser.create(numbers);
+        assertThat(parser).isInstanceOf(DefaultParser.class);
+    }
+
+    @Test public void
+    returnsCorrectImplementationForNumbersStartingWithSeperatorPrefixForSingleCharacterSeperators() {
+        String numbers = SINGLE_CHAR_CUST_SEPERATOR_PREFIX;
+        Parser parser = Parser.create(numbers);
+        assertThat(parser).isInstanceOf(SingleCharacterCustomParser.class);
+    }
+
+    @Test public void
+    returnsCorrectImplementationForNumbersStartingWithSeperatorPrefixForMultiCharacterSeperators() {
+        String numbers = MULTI_CHAR_CUST_SEPERATOR_PREFIX;
+        Parser parser = Parser.create(numbers);
+        assertThat(parser).isInstanceOf(MultiCharacterCustomParser.class);
     }
 }
